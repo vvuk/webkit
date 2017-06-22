a5c00628abc04f95443d80eca8e7f07b8c0ec8e3
Vladimir Vukicevic <vladimir@pobox.com>
2017-06-22 11:54:11 -0400

    Don't force static runtime (MT) on Windows

--- Source/cmake/OptionsMSVC.cmake
+++ Source/cmake/OptionsMSVC.cmake
@@ -55,12 +55,12 @@ foreach (flag_var
     CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
     CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
     # Use the multithreaded static runtime library instead of the default DLL runtime.
-    string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+    #string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
 
     # No debug runtime, even in debug builds.
     if (NOT DEBUG_SUFFIX)
-        string(REGEX REPLACE "/MTd" "/MT" ${flag_var} "${${flag_var}}")
-        string(REGEX REPLACE "/D_DEBUG" "" ${flag_var} "${${flag_var}}")
+        #string(REGEX REPLACE "/MTd" "/MT" ${flag_var} "${${flag_var}}")
+        #string(REGEX REPLACE "/D_DEBUG" "" ${flag_var} "${${flag_var}}")
     endif ()
 endforeach ()
 
